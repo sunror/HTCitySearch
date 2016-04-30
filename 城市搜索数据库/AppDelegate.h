1a4df3d7667648c138b9bbf32b724747128e3701
scl <sclcoder@163.com>
2016-04-30 18:04:57 +0800

    Initial Commit

---
+++ 城市搜索数据库/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  城市搜索数据库
+//
+//  Created by mac on 16/4/30.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 城市搜索数据库/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  城市搜索数据库
+//
+//  Created by mac on 16/4/30.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
